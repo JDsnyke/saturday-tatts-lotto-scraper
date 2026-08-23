2696e37cf375892ea6a8cff598a61ed755b30b73
Janith Dissanayake <owner@jdsnyke.com>
2026-08-23 18:06:15 +1000

    Route game catalog export command

--- src/lotto_lab/__main__.py
+++ src/lotto_lab/__main__.py
@@ -5,7 +5,7 @@ import sys
 from .cli import main as saturday_main
 from .game_cli import main as game_main
 
-GAME_COMMANDS = {"games", "game-odds", "keno", "cash3"}
+GAME_COMMANDS = {"games", "game-odds", "game-catalog-json", "keno", "cash3"}
 
 if len(sys.argv) > 1 and sys.argv[1] in GAME_COMMANDS:
     raise SystemExit(game_main(sys.argv[1:]))
